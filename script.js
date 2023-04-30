de6c2eba6f0ae7e9fe159252dd910593b774c412
Ahmeda Farhin <124598487+aFarhin@users.noreply.github.com>
2023-04-30 12:23:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,92 +1,87 @@
-const questions = [
-  {
-    question: "What is the capital of France?",
-    choices: ["Paris", "London", "Berlin", "Madrid"],
-    answer: "Paris",
-  },
-  {
-    question: "What is the highest mountain in the world?",
-    choices: ["Everest", "Kilimanjaro", "Denali", "Matterhorn"],
-    answer: "Everest",
-  },
-  {
-    question: "What is the largest country by area?",
-    choices: ["Russia", "China", "Canada", "United States"],
-    answer: "Russia",
-  },
-  {
-    question: "Which is the largest planet in our solar system?",
-    choices: ["Earth", "Jupiter", "Mars"],
-    answer: "Jupiter",
-  },
-  {
-    question: "What is the capital of Canada?",
-    choices: ["Toronto", "Montreal", "Vancouver", "Ottawa"],
-    answer: "Ottawa",
-  },
+// Product data
+const products = [
+  { id: 1, name: "Product 1", price: 10 },
+  { id: 2, name: "Product 2", price: 20 },
+  { id: 3, name: "Product 3", price: 30 },
+  { id: 4, name: "Product 4", price: 40 },
+  { id: 5, name: "Product 5", price: 50 },
 ];
 
-const submitButton = document.getElementById("submit");
-const questionsElement = document.getElementById("questions");
+// DOM elements
+const productList = document.getElementById("product-list");
+const cartList = document.getElementById("cart-list");
+const clearCartBtn = document.getElementById("clear-cart-btn");
 
-// Get the saved progress from session storage
-const savedProgress = JSON.parse(sessionStorage.getItem("progress"));
-// Submit the quiz and show the score
-submitButton.addEventListener("click", showScore);
+// Cart data
+let cart = [];
 
-// If there is saved progress, use it. Otherwise, start from the beginning.
-let userAnswers = savedProgress ? savedProgress : [];
-
-// Display the quiz questions and choices
-function renderQuestions() {
-  for (let i = 0; i < questions.length; i++) {
-    const question = questions[i];
-    const questionElement = document.createElement("div");
-    const questionText = document.createTextNode(question.question);
-    questionElement.appendChild(questionText);
-    for (let j = 0; j < question.choices.length; j++) {
-      const choice = question.choices[j];
-      const choiceElement = document.createElement("input");
-      choiceElement.setAttribute("type", "radio");
-      choiceElement.setAttribute("name", `question-${i}`);
-      choiceElement.setAttribute("value", choice);
-      if (userAnswers[i] === choice) {
-        choiceElement.setAttribute("checked", true);
-      }
-      const choiceText = document.createTextNode(choice);
-      questionElement.appendChild(choiceElement);
-      questionElement.appendChild(choiceText);
-    }
-    questionsElement.appendChild(questionElement);
-  }
+// Render product list
+function renderProducts() {
+  products.forEach((product) => {
+    const li = document.createElement("li");
+    li.innerHTML = `${product.name} - $${product.price} <button class="add-to-cart-btn" data-id="${product.id}">Add to Cart</button>`;
+    productList.appendChild(li);
+  });
 }
 
-// Save the user's answer when they select a choice
-function saveAnswers() {
-  const choiceElements = document.querySelectorAll("input[type=radio]");
-  for (let i = 0; i < choiceElements.length; i++) {
-    const choiceElement = choiceElements[i];
-    choiceElement.addEventListener("change", function (event) {
-      const answer = event.target.value;
-      userAnswers[parseInt(event.target.name.split("-")[1], 10)] = answer;
-      sessionStorage.setItem("progress", JSON.stringify(userAnswers));
-    });
-  }
+// Render cart list
+function renderCart() {
+  cartList.innerHTML = "";
+  cart.forEach((cartItem) => {
+    const li = document.createElement("li");
+    li.innerHTML = `${cartItem.name} - $${cartItem.price} <button class="remove-from-cart-btn" data-id="${cartItem.id}">Remove from Cart</button>`;
+    cartList.appendChild(li);
+  });
 }
 
-function showScore() {
-  let score = 0;
-  for (let i = 0; i < questions.length; i++) {
-    const question = questions[i];
-    const userAnswer = userAnswers[i];
-    if (userAnswer === question.answer) {
-      score++;
-    }
-  }
-  localStorage.setItem("score", score);
-  const scoreElement = document.getElementById("score");
-  scoreElement.innerText = `Your score is ${score} out of ${questions.length}.`;
+// Add item to cart
+function addToCart(productId) {
+  const productToAdd = products.find((product) => product.id === productId);
+  if (!productToAdd) return;
+  cart.push(productToAdd);
+  sessionStorage.setItem("cart", JSON.stringify(cart));
+  renderCart();
 }
 
-renderQuestions();
-saveAnswers();
+// Remove item from cart
+function removeFromCart(productId) {
+  const indexToRemove = cart.findIndex((cartItem) => cartItem.id === productId);
+  if (indexToRemove === -1) return;
+  cart.splice(indexToRemove, 1);
+  sessionStorage.setItem("cart", JSON.stringify(cart));
+  renderCart();
+}
+
+// Clear cart
+function clearCart() {
+  cart = [];
+  sessionStorage.removeItem("cart");
+  renderCart();
+}
+
+// Event listeners
+productList.addEventListener("click", (event) => {
+  if (event.target.classList.contains("add-to-cart-btn")) {
+    const productId = parseInt(event.target.dataset.id);
+    addToCart(productId);
+  }
+});
+
+cartList.addEventListener("click", (event) => {
+  if (event.target.classList.contains("remove-from-cart-btn")) {
+    const productId = parseInt(event.target.dataset.id);
+    removeFromCart(productId);
+  }
+});
+
+clearCartBtn.addEventListener("click", clearCart);
+
+// Initial render
+renderProducts();
+
+// Check if cart data is in session storage and render cart
+const cartData = sessionStorage.getItem("cart");
+if (cartData) {
+  cart = JSON.parse(cartData);
+  renderCart();
+}
